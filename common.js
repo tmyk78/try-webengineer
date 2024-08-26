656f1a39a44393696d5b28c0b7c797000bbd344a
tmyk <tmyk78@gmail.com>
2024-08-27 01:33:57 +0900

    css、jsファイル修正（途中）

--- common.js
+++ common.js
@@ -1,1 +1,10 @@
-console.log(123);
+document.addEventListener('DOMContentLoaded', () => {
+
+    // タイトルに改行を入れる
+    const titleElement = document.querySelector('#title');
+    const textNode = titleElement.querySelector('a').firstChild;
+    const parts = textNode.nodeValue.split('！');
+    const newHTML = `${parts[0]}！<br>${parts[1]}`;
+    titleElement.querySelector('a').innerHTML = newHTML;
+  });
+  
