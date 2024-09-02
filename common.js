d91cfd6b83fa31e8278e385c3e61265d8f568217
Tomoyuki Akimoto <tmyk78@gmail.com>
2024-09-02 22:54:53 +0900

    sp確認のため一旦コミット

--- common.js
+++ common.js
@@ -1,10 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // タイトルに改行を入れる
-    const titleElement = document.querySelector('#title');
-    const textNode = titleElement.querySelector('a').firstChild;
-    const parts = textNode.nodeValue.split('！');
-    const newHTML = `${parts[0]}！<br>${parts[1]}`;
-    titleElement.querySelector('a').innerHTML = newHTML;
-  });
+  // タイトルに改行を入れる
+  const titleElement = document.querySelector('#title');
+  const textNode = titleElement.querySelector('a').firstChild;
+  const parts = textNode.nodeValue.split('！');
+  const newHTML = `${parts[0]}！<br>${parts[1]}`;
+  titleElement.querySelector('a').innerHTML = newHTML;
+
+  // コメントにタイトルとコメントを追加する
+  const newCommentHtml = `
+    <p class="comment-box__title">ご質問・お悩み相談受付中！</p>
+    <p class="comment-box__description">ご質問やお悩み、お気軽にコメントください！<br />
+      あなたの疑問や不安にお答えします。<br />
+      <span class="comment-box__caution">※いただいたコメントを元に記事を作成することがございますので、あらかじめご了承ください。</span>
+    </p>
+  `;
+  const commentBox = document.querySelector('.js-comment-box');
+  commentBox.insertAdjacentHTML('afterbegin', newCommentHtml);
   
+});
