ca72c226b18b8dff61200963b47d251b31d1e057
Tomoyuki Akimoto <tmyk78@gmail.com>
2024-09-06 01:06:17 +0900

    確認用ファイル再アップ

--- common.js
+++ common.js
@@ -9,12 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // コメントにタイトルとコメントを追加する
   const newCommentHtml = `
-    <p class="comment-box__title">ご質問・お悩み相談受付中！</p>
+    <p class="illustrationTitle comment-box__title">ご質問・お悩み相談受付中！</p>
     <p class="comment-box__description">ご質問やお悩み、お気軽にコメントください！<br />
       あなたの疑問や不安にお答えします。<br />
       <span class="comment-box__caution">※いただいたコメントを元に記事を作成することがございますので、あらかじめご了承ください。</span>
     </p>
   `;
-  document.querySelector('.js-comment-box').insertAdjacentHTML('afterbegin', newCommentHtml);
+  const commentBox = document.querySelector('.js-comment-box');
+  if(commentBox) {
+    commentBox.insertAdjacentHTML('afterbegin', newCommentHtml);
+  }
+  
   
 });
